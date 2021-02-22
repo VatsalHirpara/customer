670ddede2122d11a524fc1fbded80eb225771a2f
Vatsal Hirpara <vatsalplay9@gmail.com>
2021-02-23 00:50:21 +0530

    Create Notification.java

---
+++ src/main/java/com/nagarro/customer/domain/Notification.java
@@ -0,0 +1,13 @@
+package com.nagarro.customer.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+	private String title;
+	private String text;
+}
